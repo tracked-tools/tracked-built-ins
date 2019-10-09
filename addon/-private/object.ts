837bb43be961782234dc3245d819fa03e09f7c2d
Aleksandr Kanunnikov <lifeart92@gmail.com>
2019-10-09 19:46:34 +0300

    track object properties without proxy

--- addon/-private/object.ts
+++ addon/-private/object.ts
@@ -1,31 +1,77 @@
 import { assert } from '@ember/debug';
-
+import { DEBUG } from '@glimmer/env';
 import { USE_PROXY } from './config';
 import { consume, dirty, SUPPORTS_PROXY } from './util';
 
-export default class TrackedObject extends Object {
-  constructor(obj: object) {
-    super();
+const setterPropertyPrefix = '__PROPERTY__';
 
-    Object.assign(this, obj);
+function defineHiddenProperty(obj: object, propertyName: string, value: any) {
+  Object.defineProperty(obj, propertyName, {
+    enumerable: false,
+    configurable: true,
+    writable: true,
+    value
+  })
+}
+
+function patchObjectDescriptor(obj: object, keyName: string, desc: PropertyDescriptor) {
+  if (desc.configurable === false) {
+    return;
+  }
+  if (typeof desc.get === 'function' || typeof desc.set === 'function') {
+    return;
+  }
+  const key = `${setterPropertyPrefix}${keyName}`;
+  defineHiddenProperty(obj, key, desc.value);
+  Object.defineProperty(obj, keyName, {
+    enumerable: desc.enumerable,
+    configurable: true,
+    get() {
+      consume(this, keyName);
+      return (this as any)[key];
+    },
+    set(value) {
+      dirty(this, keyName);
+      (this as any)[key] = value;
+    }
+  });
+}
+
+const proxyHandler = {
+  get(target: object, prop: string) {
+    consume(target, prop);
+    return Reflect.get(target, prop);
+  },
+
+  set(target: object, prop: string, value: any) {
+    dirty(target, prop);
+    return Reflect.set(target, prop, value);
+  }
+}
+
+export default class TrackedObject extends Object {
+  constructor(obj: object = {}) {
+    super();
 
     assert(
       'You attempted to create a TrackedObject, but you have not enabled Proxy support or you are using a browser that does not support Proxy.',
-      SUPPORTS_PROXY && USE_PROXY
+      USE_PROXY ? SUPPORTS_PROXY : true
     );
+    
+    Object.assign(this, obj);
 
-    return new Proxy(this, {
-      get(target, prop) {
-        consume(self, prop);
+    if (USE_PROXY === false) {
+      Object.keys(obj).forEach((keyName)=>{
+        const desc: PropertyDescriptor = Object.getOwnPropertyDescriptor(obj, keyName) as PropertyDescriptor;
+        patchObjectDescriptor(this, keyName, desc);
+      });
+      if (DEBUG) {
+        return Object.seal(this);
+      } else {
+        return this;
+      }
+    }
 
-        return Reflect.get(target, prop);
-      },
-
-      set(target, prop, value) {
-        dirty(self, prop);
-
-        return Reflect.set(target, prop, value);
-      },
-    });
+    return new Proxy(this, proxyHandler);
   }
 }
--- tests/unit/object-test.js
+++ tests/unit/object-test.js
@@ -9,17 +9,36 @@ module('TrackedObject', function(hooks) {
   setupRenderingTest(hooks);
 
   module('USE_PROXY: false', () => {
-    test('throws if proxy is not available', assert => {
-      assert.throws(
-        () => new TrackedObject(),
-        /You attempted to create a TrackedObject, but you have not enabled Proxy support/
-      );
+    hooks.beforeEach(() => setTrackedBuiltInsConfig({ USE_PROXY: false }));
+    hooks.afterEach(() => setTrackedBuiltInsConfig({ USE_PROXY: false }));
+
+    test('from', assert => {
+      let obj = new TrackedObject({ foo: 123 });
+
+      assert.ok(obj instanceof TrackedObject);
+      assert.deepEqual(Object.keys(obj), ['foo']);
     });
+
+    reactivityTest(
+      'it works',
+      class extends Component {
+        obj = new TrackedObject({foo: undefined});
+
+        get value() {
+          return this.obj.foo;
+        }
+
+        update() {
+          this.obj.foo = 123;
+        }
+      },
+      true
+    );
   });
 
   module('USE_PROXY: true', hooks => {
     hooks.beforeEach(() => setTrackedBuiltInsConfig({ USE_PROXY: true }));
-    hooks.afterEach(() => setTrackedBuiltInsConfig({ USE_PROXY: false }));
+    hooks.afterEach(() => setTrackedBuiltInsConfig({ USE_PROXY: true }));
 
     test('from', assert => {
       let obj = new TrackedObject({ foo: 123 });
@@ -40,7 +59,8 @@ module('TrackedObject', function(hooks) {
         update() {
           this.obj.foo = 123;
         }
-      }
+      },
+      true
     );
   });
 });
