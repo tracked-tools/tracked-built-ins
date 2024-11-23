442f1ac576aa0377892f04f69d34beaf798e3dd9
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-11-22 22:01:07 -0500

    Bring back ember 3.28+ support

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -7,6 +7,28 @@ module.exports = async function () {
     usePnpm: true,
     scenarios: [
       {
+        name: 'ember-lts-3.28',
+        npm: {
+          devDependencies: {
+            '@ember/string': '^3.0.0',
+            '@ember/test-helpers': '^2.9.3',
+            '@glimmer/component': '^1.0.0',
+            'ember-cli': '~3.28.0',
+            'ember-qunit': '^6.0.0',
+            'ember-resolver': '~9.0.0',
+            'ember-source': '~3.28.11',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-4.12',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.12.0',
+          },
+        },
+      },
+      {
         name: 'ember-lts-5.4',
         npm: {
           devDependencies: {
