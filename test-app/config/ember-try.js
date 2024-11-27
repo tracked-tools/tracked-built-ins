2899c6c083f36dd5c6fb5b195cfafb9b77a79cd8
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-11-27 15:25:04 -0500

    Re-add support for 3.24

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -2,10 +2,24 @@
 
 const getChannelURL = require('ember-source-channel-url');
 
-module.exports = async function () {
+module.exports = async function() {
   return {
     usePnpm: true,
     scenarios: [
+      {
+        name: 'ember-lts-3.24',
+        npm: {
+          devDependencies: {
+            '@ember/string': '^3.0.0',
+            '@ember/test-helpers': '^2.9.3',
+            '@glimmer/component': '^1.0.0',
+            'ember-cli': '~3.28.0',
+            'ember-qunit': '^6.0.0',
+            'ember-resolver': '~8.0.0',
+            'ember-source': '~3.24.0',
+          },
+        },
+      },
       {
         name: 'ember-lts-3.28',
         npm: {
